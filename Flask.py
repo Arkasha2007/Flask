ae0f1ec342b9420690e13903083060cc66368f48
4 задача классная <arkadiikrasnodar@gmail.com>
2025-03-07 19:38:20 +0300

    Четвертая задача классная

---
+++ Flask.py
@@ -0,0 +1,101 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def index():
+    return '<h1>И на Марсе будут яблони цвести!<h1>'
+
+
+@app.route('/')
+def main():
+    return '<h1>Миссия Колонизация Марса<h1>'
+
+
+# @app.route('/promotion')
+# def result():
+#     results = ['Человечество вырастает из детства.',
+#                'Человечеству мала одна планета.',
+#                'Мы сделаем обитаемыми безжизненные пока планеты.',
+#                'И начнем с Марса!',
+#                'Присоединяйся!']
+#     return '''<!DOCTYPE html>
+#             <html lang="en">
+#             <head>
+#                 <meta charset="UTF-8">
+#                 <title>Title</title>
+#             </head>
+#             <body>
+#                 <h3>{}</h3>
+#                 <h3>{}</h3>
+#                 <h3>{}</h3>
+#                 <h3>{}</h3>
+#                 <h3>{}</h3>
+#             </body>
+#             </html>'''.format(*results)
+
+
+@app.route('/sample_page')
+def return_sample_page():
+    results = ['Человечество вырастает из детства.',
+               'Человечеству мала одна планета.',
+               'Мы сделаем обитаемыми безжизненные пока планеты.',
+               'И начнем с Марса!',
+               'Присоединяйся!']
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1 style="color:#DC143C;">Жди нас, Марс!</h1>
+                    <img src="/static/img/Mars.png" alt="здесь должна была быть картинка, но не нашлась">
+                    <div class="alert alert-dark" role="alert">
+                        <h3>{results[0]}</h3>
+                    </div>
+                     <div class="alert alert-success" role="alert">
+                        <h3>{'Бездельник'}</h3>
+                    </div>
+                    <div class="alert alert-dark" role="alert">
+                        <h3>{results[2]}</h3>
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                        <h3>{results[3]}</h3>
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                        <h3>{results[4]}</h3>
+                    </div>
+                  </body>
+                </html>"""
+
+
+# @app.route('/bootstrap_sample')
+# def bootstrap():
+#     return '''<!doctype html>
+#                 <html lang="en">
+#                   <head>
+#                     <meta charset="utf-8">
+#                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+#                     <link rel="stylesheet"
+#                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+#                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+#                     crossorigin="anonymous">
+#                     <title>Привет, Яндекс!</title>
+#                   </head>
+#                   <body>
+#                     <h1>Привет, Яндекс!</h1>
+#                     <div class="alert alert-primary" role="alert">
+#                       А мы тут компонентами Bootstrap балуемся
+#                     </div>
+#                   </body>
+#                 </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
