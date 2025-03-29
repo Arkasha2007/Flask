a700c647a378d209816bc964a4a32bee90c536b4
Arkasha2007 <arkadiikrasnodar@gmail.com>
2025-03-29 09:25:18 +0300

    2 урок 4 классная задача

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/training/<prof>')
-def odd_even(prof):
-    return render_template('index.html', username=prof)
+@app.route('/form_sample', methods=['POST', 'GET'])
+def form():
+    return render_template('First_file.html')
+
+
+@app.route('/auto_answer', methods=['GET', 'POST'])
+@app.route('/answer', methods=['GET', 'POST'])
+def answer():
+    return render_template('index.html', say=request.form['surname'], title=request.form['name'],
+                           education=request.form['student_courses'], profession=request.form['user_profession'],
+                           sex=request.form['user_age'], motivation=request.form['student_bio'],
+                           ready=request.form['approval'])
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run()
