cea40b0cd6661a669773061b37dac68d1564f16d
Arkasha2007 <arkadiikrasnodar@gmail.com>
2025-04-10 15:18:42 +0300

    Последняя задача классная 1 урок flask-sqlalchemy

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, render_template
+from data import db_session
+from data.jobs import Jobs
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/form_sample', methods=['POST', 'GET'])
-def form():
-    return render_template('First_file.html')
+@app.route('/')
+def index():
+    session = db_session.create_session()
+    jobs = session.query(Jobs).all()
+    return render_template("index.html", jobs=jobs)
 
 
-@app.route('/auto_answer', methods=['GET', 'POST'])
-@app.route('/answer', methods=['GET', 'POST'])
-def answer():
-    return render_template('index.html', say=request.form['surname'], title=request.form['name'],
-                           education=request.form['student_courses'], profession=request.form['user_profession'],
-                           sex=request.form['user_age'], motivation=request.form['student_bio'],
-                           ready=request.form['approval'])
+def main():
+    db_session.global_init('db/mars.db')
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run()
+    main()
